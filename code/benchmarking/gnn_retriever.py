670a8388b35e5d5aacd6fe80f679068006c34319
Saptarshi <ssengupta8@gmail.com>
2025-05-08 17:24:50 -0400

    graph_ret

--- code/benchmarking/gnn_retriever.py
+++ code/benchmarking/gnn_retriever.py
@@ -62,7 +62,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class GCN(nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, vector_emb_dim, graph_type):
         super().__init__()
-        if graph_type == "Conv":
+        if graph_type == "GCN":
             print("Building GCNConv model")
             self.conv1 = GCNConv(in_channels, hidden_channels)
             self.conv2 = GCNConv(hidden_channels, out_channels)
@@ -71,7 +71,7 @@ class GCN(nn.Module):
             self.conv1 = SAGEConv(in_channels, hidden_channels)
             self.conv2 = SAGEConv(hidden_channels, out_channels)
         else:
-            print("Building SAGEConv model")
+            print("Building GATConv model")
             heads = args.heads
             self.conv1 = GATConv(in_channels, hidden_channels, heads=heads)
             self.conv2 = GATConv(hidden_channels * heads, out_channels, heads=1)
