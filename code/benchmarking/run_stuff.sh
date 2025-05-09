40b21771cf6c813fd20ba8791a220826aa3b3b38
Saptarshi <ssengupta8@gmail.com>
2025-05-09 12:47:09 -0400

    gnn_ret

--- code/benchmarking/run_stuff.sh
+++ code/benchmarking/run_stuff.sh
@@ -4,6 +4,6 @@ export CUDA_VISIBLE_DEVICES=2
 
 for model in "GCN" "SAGE" "GAT"; do
     for i in {1..3}; do
-        python gnn_retriever.py --graph_type $model --epochs 15 --run_number $i
+        python gnn_retriever.py --graph_type $model --run_number $i
     done
 done
