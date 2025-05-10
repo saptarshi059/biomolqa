9ff7782f696a913dea3ea5e5f1b218abccd8bc90
Saptarshi <ssengupta8@gmail.com>
2025-05-09 21:25:08 -0400

    gnn-ret fix

--- code/benchmarking/gnn_retriever.py
+++ code/benchmarking/gnn_retriever.py
@@ -6,7 +6,7 @@ from torch_geometric.data import Data
 from collections import defaultdict
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GCNConv, SAGEConv, GATConv, GraphConv
+from torch_geometric.nn import GCNConv, SAGEConv, GATConv
 from transformers import AutoModel, AutoTokenizer
 from torch.utils.data import Dataset, DataLoader
 import random
@@ -70,17 +70,17 @@ class GCN(nn.Module):
         super().__init__()
         if graph_type == "GCN":
             print("Building GCNConv model")
-            self.conv1 = GraphConv(in_channels, hidden_channels)
-            self.conv2 = GraphConv(hidden_channels, out_channels)
+            self.conv1 = GCNConv(in_channels, out_channels)
+            #self.conv2 = GCNConv(hidden_channels, out_channels)
         elif graph_type == "SAGE":
             print("Building SAGEConv model")
-            self.conv1 = SAGEConv(in_channels, hidden_channels)
-            self.conv2 = SAGEConv(hidden_channels, out_channels)
+            self.conv1 = SAGEConv(in_channels, out_channels)
+            #self.conv2 = SAGEConv(hidden_channels, out_channels)
         else:
             print("Building GATConv model")
             heads = args.heads
-            self.conv1 = GATConv(in_channels, hidden_channels, heads=heads)
-            self.conv2 = GATConv(hidden_channels * heads, out_channels, heads=1)
+            self.conv1 = GATConv(in_channels, out_channels, heads=heads)
+            #self.conv2 = GATConv(hidden_channels * heads, out_channels, heads=1)
        
         self.linear = nn.Linear(out_channels, vector_emb_dim)
         self.relu = nn.ReLU()
@@ -90,7 +90,7 @@ class GCN(nn.Module):
         x, edge_index = graph.x.to(device), graph.edge_index.to(device)
         x = self.conv1(x, edge_index)
         x = self.relu(x)
-        x = self.conv2(x, edge_index)
+        #x = self.conv2(x, edge_index)
         x = self.linear(x)
         x = self.relu(x)
         return x  # Node embeddings: [num_nodes, D]
