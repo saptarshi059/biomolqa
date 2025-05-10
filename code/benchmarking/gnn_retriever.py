413115bd717068f3b5b4690b6738eb43769ba6cd
Saptarshi <ssengupta8@gmail.com>
2025-05-09 22:32:42 -0400

    gnn-ret fix

--- code/benchmarking/gnn_retriever.py
+++ code/benchmarking/gnn_retriever.py
@@ -330,7 +330,7 @@ def get_corrupt_tail_ids(head_ids, rel_ids, tail_ids, num_entities, existing_tri
 
 optimizer = torch.optim.AdamW(list(gcn.parameters()) + list(predictor.parameters()))
 
-for epoch in range(epochs):
+for epoch in tqdm(range(args.epochs)):
     gcn.train()
     predictor.train()
     epoch_loss = 0.0
