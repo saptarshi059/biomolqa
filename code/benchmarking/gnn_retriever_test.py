59c21c8baae26fd249c55cdf81cdde61a181db65
Saptarshi <ssengupta8@gmail.com>
2025-05-10 01:06:36 -0400

    gnn-ret fix

--- code/benchmarking/gnn_retriever_test.py
+++ code/benchmarking/gnn_retriever_test.py
@@ -16,13 +16,8 @@ import numpy
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--learning_rate", default=1e-4,type=float)
-parser.add_argument("--graph_type", default="GCN", type=str)
 parser.add_argument("--batch_size", default=64, type=int)
-parser.add_argument("--epochs", default=10, type=int)
-parser.add_argument("--heads",default=1,type=int)
-parser.add_argument("--run_number", default=1, type=int)
-parser.add_argument("--query_embedding_model", default='sentence-transformers/all-MiniLM-L12-v2', type=str)
+parser.add_argument("--saved_model_path")
 args = parser.parse_args()
 
 class GCN(nn.Module):
@@ -78,9 +73,7 @@ class QueryEncoder(nn.Module):
         out = self.bert(**encoded.to(device))
         return out.last_hidden_state[:, 0, :]  # [CLS] token
 
-
-
-class ValidationGraphDataset(Dataset):
+class TestGraphDataset(Dataset):
     def __init__(self, df):
         self.questions = df.Question.tolist()
         self.gold_triples = df.Gold_Triples.tolist()
@@ -93,5 +86,86 @@ class ValidationGraphDataset(Dataset):
         positive_triples = self.gold_triples[idx].tolist()
         return question_emb, positive_triples
 
+gcn = torch.load(f"{args.saved_model_path}/gcn.pt", weights_only=False)
+query_encoder = torch.load(f"{args.saved_model_path}/query_encoder.pt", weights_only=False)
+triple_encoder = torch.load(f"{args.saved_model_path}/triple_encoder.pt", weights_only=False)
 
-model = torch.load("saved_models/GCN/run_number_1/gcn.pt", weights_only=False)
+tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L12-v2')
+
+test_df = pd.read_parquet("../../data/mined_data/test_gold.parquet")
+
+gcn.eval()
+query_encoder.eval()
+triple_encoder.eval()
+
+full_graph = pd.read_csv("../../data/mined_data/full_graph.csv")
+triples = 
+
+
+
+hard_hits_5 = []
+soft_hits_5 = []
+
+hard_hits_10 = []
+soft_hits_10 = []
+
+hard_hits_15 = []
+soft_hits_15 = []
+
+recall_5 = []
+mrr = []
+
+for row in tqdm(test_df.itertuples()):
+	with torch.no_grad():
+		node_embeddings = gcn(graph)  # [num_nodes, D]
+        query_emb = batch[0].squeeze(1)  # [B, D]
+
+        all_triple_embeds = triple_encoder(
+            all_head_ids, all_rel_ids, all_tail_ids, node_embeddings
+        )  # [N_triples, D]
+
+        query_emb = nn.functional.normalize(query_emb, dim=-1)
+        all_triple_embeds = nn.functional.normalize(all_triple_embeds, dim=-1)
+        
+        sims = torch.matmul(query_emb, all_triple_embeds.T)
+        
+        top5 = torch.topk(sims, k=5)
+        top5_indices = top5.indices.tolist()
+        for idx, index_list in enumerate(top5_indices):
+          predictions = [] 
+          for index in index_list:
+            predictions.append((H[index], R[index], T[index]))
+          hard_hits_5.append(hard_hits(predictions, batch[1][idx]))
+          soft_hits_5.append(soft_hits(predictions, batch[1][idx]))
+          recall_5.append(recall(predictions, batch[1][idx]))
+          mrr.append(mrr_calc(predictions, batch[1][idx]))
+
+        top10 = torch.topk(sims, k=10)
+        top10_indices = top10.indices.tolist()
+        for idx, index_list in enumerate(top10_indices):
+          predictions = []
+          for index in index_list:
+            predictions.append((H[index], R[index], T[index]))
+          hard_hits_10.append(hard_hits(predictions, batch[1][idx]))
+          soft_hits_10.append(soft_hits(predictions, batch[1][idx]))
+
+        top15 = torch.topk(sims, k=15)
+        top15_indices = top15.indices.tolist()
+        for idx, index_list in enumerate(top15_indices):
+          predictions = []
+          for index in index_list:
+            predictions.append((H[index], R[index], T[index]))
+          hard_hits_15.append(hard_hits(predictions, batch[1][idx]))
+          soft_hits_15.append(soft_hits(predictions, batch[1][idx]))
+
+print(f"Hard hits@5: {sum(hard_hits_5)/len(hard_hits_5):.2f}")
+print(f"Soft hits@5: {sum(soft_hits_5)/len(soft_hits_5):.2f}")
+
+print(f"Hard hits@10: {sum(hard_hits_10)/len(hard_hits_10):.2f}")
+print(f"Soft hits@10: {sum(soft_hits_10)/len(soft_hits_10):.2f}")
+
+print(f"Hard hits@15: {sum(hard_hits_15)/len(hard_hits_15):.2f}")
+print(f"Soft hits@15: {sum(soft_hits_15)/len(soft_hits_15):.2f}")
+
+print(f"recall@5: {sum(recall_5)/len(recall_5):.2f}")
+print(f"mrr: {sum(mrr)/len(mrr):.2f}")
--- code/benchmarking/run_stuff.sh
+++ code/benchmarking/run_stuff.sh
@@ -2,8 +2,6 @@
 
 export CUDA_VISIBLE_DEVICES=2
 
-for model in "GCN" "SAGE" "GAT"; do
-    for i in {1..3}; do
-        python gnn_retriever.py --graph_type $model --run_number $i
-    done
-done
+python gnn_retriever.py --graph_type "GCN"
+python gnn_retriever.py --graph_type "SAGE"
+python gnn_retriever.py --graph_type "GAT"
